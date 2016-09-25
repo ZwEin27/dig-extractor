d198fe16d389a872bedbac72fe1bfb9a61e52bec
ZwEin <zwein27@gmail.com>
2016-09-25 15:55:00 -0700

    add load_dictionary_dir()

--- dige/dict_loader.py
+++ dige/dict_loader.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-09-25 14:55:45
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-09-25 15:20:14
+# @Last Modified time: 2016-09-25 15:54:50
 
 import os
 import sys
@@ -12,13 +12,30 @@ import faerie
 
 DICT_PATH = os.path.join(os.path.dirname(__file__), '..', 'dictionaries')
 
-def load_dictionary(path):
-    with codecs.open(path, 'r', 'utf-8') as file_handler:
+def load_dictionary_file(filepath):
+    with codecs.open(filepath, 'r', 'utf-8') as file_handler:
         return [_.strip() for _ in file_handler.readlines()]
+
+def load_dictionary_dir(dirpath):
+    ans = []
+    for filename in os.listdir(dirpath):
+        print filename
+        if filename[0] == '.':
+            continue
+        if filename.split('.')[-1] != 'txt':
+            continue
+
+        path = os.path.join(dirpath, filename)
+        ans += load_dictionary_file(path)
 
 if __name__ == '__main__':
 
-    path = os.path.join(DICT_PATH, 'person_name', 'female_irish.txt')
-    print load_dictionary(path)
+    # # load file
+    # path = os.path.join(DICT_PATH, 'person_name', 'female_irish.txt')
+    # print load_dictionary_file(path)
 
+
+    # load dir
+    path = os.path.join(DICT_PATH, 'person_name')
+    print load_dictionary_dir(path)
 
