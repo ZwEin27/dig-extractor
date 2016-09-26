1842d88ee5efb16d6104e4b734e3d4cf2a6930d7
ZwEin <zwein27@gmail.com>
2016-09-25 20:20:06 -0700

    add test_sampling

--- tests/test_faerie.py
+++ tests/test_faerie.py
@@ -14,26 +14,56 @@ from dige.extractor import faerie_extractor
 class TestFaerieMethods(unittest.TestCase):
     def setUp(self):
         self.gt_filepath = os.path.join(GT_DATA_DIR, 'all_extractions_july_2016.jl')
+        self.gt_filepath_sampled = os.path.join(GT_DATA_DIR, 'all_extractions_july_2016_50.jl')
+        self.gt_filepath_annotated = os.path.join(GT_DATA_DIR, 'all_extractions_july_2016_50_annotated.jl')
 
     def tearDown(self):
         pass
 
-    def test_faerie(self):
+    def test_sampling(self):
         ans = []
         with codecs.open(self.gt_filepath, 'r', 'utf-8') as file_handler:
             for line in file_handler.readlines():
-                line = json.loads(line.strip())
+                line = json.loads(line.strip().lower())
+                content = line['content']
+
+                if not ('my name' in content.lower() or 'I\'m' in content.lower()):
+                    continue
+
+                annotation = faerie_extractor.run4text(content)
+                annotation = faerie_extractor.clean_extraction(annotation)
+                if annotation:
+                    ans.append(line)
+
+
+        with codecs.open(self.gt_filepath_sampled, 'w', 'utf-8') as file_handler:
+            for item in ans:
+                file_handler.write(json.dumps(item) + '\n')
+
+    def test_faerie(self):
+        ans = []
+        with codecs.open(self.gt_filepath_sampled, 'r', 'utf-8') as file_handler:
+            for line in file_handler.readlines():
+                line = json.loads(line.strip().lower())
                 annotation = faerie_extractor.run4text(line['content'])
                 annotation = faerie_extractor.clean_extraction(annotation)
-                line.setdefault('annotated_names', annotation)
-                ans.append(line)
-        print ans
+                if annotation:
+                    line.setdefault('annotated_names', annotation)
+                    ans.append(line)
+
+        # ans = [_ for _ in ans if _['annotated_names']]
+        # print json.dumps(ans, indent=4)
+
+        with codecs.open(self.gt_filepath_annotated, 'w', 'utf-8') as file_handler:
+            for item in ans:
+                file_handler.write(json.dumps(item) + '\n')
 
 if __name__ == '__main__':
     # unittest.main()
 
     def run_main_test():
         suite = unittest.TestSuite()
+        # suite.addTest(TestFaerieMethods('test_sampling'))
         suite.addTest(TestFaerieMethods('test_faerie'))
         
         runner = unittest.TextTestRunner()
