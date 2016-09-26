cd443568c8168a6b88615f09d0d5ec1abeee4b0d
ZwEin <zwein27@gmail.com>
2016-09-25 20:20:33 -0700

    clean up

--- tests/data_generator.py
+++ tests/data_generator.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-09-16 15:20:49
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-09-25 19:13:23
+# @Last Modified time: 2016-09-25 20:07:02
 
 import os
 import sys
@@ -40,4 +40,4 @@ def gt_template_generator(input_path, output_path=None, sample_num=None, sample_
 if __name__ == '__main__':
     input_path = os.path.join(os.path.dirname(__file__), 'data', 'all_extractions_july_2016.csv')
     output_path = os.path.join(os.path.dirname(__file__), 'data', 'ground_truth.json')
-    gt_template_generator(input_path, output_path=output_path, sample_num=50)
+    gt_template_generator(input_path, output_path=output_path, sample_num=None)
