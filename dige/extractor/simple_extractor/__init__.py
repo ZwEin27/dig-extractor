9934b53e42b1fb7df2fe727cbb8c7be4ed03e62e
ZwEin <zwein27@gmail.com>
2016-09-29 22:06:19 -0700

    add baseline extractor

--- dige/extractor/simple_extractor/__init__.py
+++ dige/extractor/simple_extractor/__init__.py
@@ -2,24 +2,30 @@
 # @Author: ZwEin
 # @Date:   2016-09-25 15:21:54
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-09-28 14:06:12
+# @Last Modified time: 2016-09-28 14:12:09
 
 
 import faerie
 import os
 import json
 import codecs
+from nltk.tokenize import word_tokenize
 
-def run4text(content):
-    
+def run4text(content, dictionary):
 
-    return jsonline
+    ans = []
+    tokens = word_tokenize(content)
+    for token in tokens:
+        if token.lower() in dictionary:
+            ans.append(token)
+    return ans
 
-def run4path(path):
+def run4path(path, dictionary):
+    extraction = []
     with codecs.open(path, 'r', 'utf-8') as file_handler:
         for line in file_handler.readlines():
-            extraction = run4text(line.strip())
-            yield extraction
-
+            extraction += run4text(line.strip(), dictionary)
+    return extraction
+    
 if __name__ == '__main__':
     print run4path(documents_)
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -10,11 +10,14 @@ TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 GT_DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'ground_truth')
 
 from dige.extractor import simple_extractor
+from dige import dict_loader
 
 class TestSimpleMethods(unittest.TestCase):
     def setUp(self):
         self.gt_filepath_sampled = os.path.join(GT_DATA_DIR, 'all_extractions_july_2016_50.jl')
         self.gt_filepath_annotated = os.path.join(GT_DATA_DIR, 'all_extractions_july_2016_50_annotated.jl')
+
+        self.names = dict_loader.load_dictionary_dir(os.path.join(dict_loader.DICT_PATH, 'person_name'))
 
     def tearDown(self):
         pass
@@ -26,10 +29,12 @@ class TestSimpleMethods(unittest.TestCase):
             for line in file_handler.readlines():
                 line = json.loads(line.strip().lower())
                 
-                annotation = simple_extractor.run4text(line['content'])
+                annotation = simple_extractor.run4text(line['content'], self.names)
                 if annotation:
                     line.setdefault('annotated_names', annotation)
-                    ans.append(line)
+                else:
+                    print 'get no annotation: ', line
+                ans.append(line)
 
                 correct_names = line['correct_names']
 
@@ -37,8 +42,12 @@ class TestSimpleMethods(unittest.TestCase):
                 for anno in annotation:
                     if anno in correct_names:
                         count += 1
+                if count == 0:
+                    print '-'*20
+                    print 'correct_names:', correct_names 
+                    print 'annotations:', annotation 
                 report.append(count)
-                break
+                # break
 
         # 33/50
         print len([_ for _ in report if _]), 'out of', len(report), 'extracted'
